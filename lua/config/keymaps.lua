3e5c0a101f398f456e57f2bcb9fd1e8aca8443ea
Harsh Dabas <iamharshdabas@gmail.com>
2024-05-30 22:52:42 +0530

    chore: catch up to the updates

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 local set = vim.keymap.set
+local map = LazyVim.safe_keymap_set
 
 set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "replace" })
+map("n", "<S-x>", LazyVim.ui.bufremove, { desc = "Delete Buffer" })
--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -1,5 +1,4 @@
 return {
   "Wansmer/treesj",
-  keys = { { "J", "<cmd>TSJToggle<cr>", desc = "Join Toggle" } },
   config = true,
 }

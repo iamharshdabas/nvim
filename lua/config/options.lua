a01f92039cefa23cc5064d18402a699c97fc4259
Harsh Dabas <iamharshdabas@gmail.com>
2025-01-04 20:22:38 +0530

    fix: neovide font size according to hyprland auto monitor scale

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,7 +4,8 @@ local g = vim.g
 opt.cursorline = false
 opt.sidescrolloff = 16
 opt.scrolloff = 8
-opt.guifont = "FiraCode Nerd Font:h16"
+opt.guifont = "FiraCode Nerd Font Mono:h14"
+
 g.neovide_transparency = 0.8
 g.neovide_hide_mouse_when_typing = true
 g.neovide_cursor_trail_size = 0.8

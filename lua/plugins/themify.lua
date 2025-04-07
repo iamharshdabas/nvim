f0de8b87c3d21686c808a543afee930a233c0585
Harsh Dabas <iamharshdabas@gmail.com>
2025-04-07 11:39:27 +0530

    feat: update themes

--- lua/plugins/themify.lua
+++ lua/plugins/themify.lua
@@ -1,3 +1,5 @@
+-- stylua: ignore
+
 return {
   "lmantw/themify.nvim",
 
@@ -7,45 +9,49 @@ return {
   config = {
     async = true,
 
+    -- aurora
+    {"ray-x/aurora", blacklist = {}},
     -- catppuccin
-    {
-      "catppuccin/nvim",
-      blacklist = { "catppuccin", "catppuccin-latte" },
-    },
+    { "catppuccin/nvim", blacklist = { "catppuccin", "catppuccin-latte" } },
+    -- cyberdream
+    {"scottmckendry/cyberdream.nvim", blacklist = {"cyberdream-light"}},
+    -- darkearth
+    {"ptdewey/darkearth-nvim", blacklist = {}},
     -- gruvbox
-    {
-      "ellisonleao/gruvbox.nvim",
-      blacklist = {},
-    },
+    { "ellisonleao/gruvbox.nvim", blacklist = {} },
     -- kanagawa
-    {
-      "rebelot/kanagawa.nvim",
-      blacklist = { "kanagawa-lotus" },
-    },
+    {"rebelot/kanagawa.nvim", blacklist = {"kanagawa-lotus"}},
+    -- kanagawa
+    {"thesimonho/kanagawa-paper.nvim", blacklist = {"kanagawa-paper-canvas", "kanagawa-paper-ink"}},
+    -- lackluster
+    {"slugbyte/lackluster.nvim", blacklist = {"lackluster"}},
+    -- melange
+    {"savq/melange-nvim", blacklist = {}},
+    -- mellow
+    {"mellow-theme/mellow.nvim", blacklist = {}},
+    -- neofusion
+    {"diegoulloao/neofusion.nvim", blacklist = {}},
+    -- neosolarized
+    {"Tsuzat/NeoSolarized.nvim", blacklist = {}},
+    -- nightcity
+    {"cryptomilk/nightcity.nvim", blacklist = {"nightcity"}},
+    -- nordic
+    {"AlexvZyl/nordic.nvim", blacklist = {}},
+    -- ohlucy
+    {"yazeed1s/oh-lucy.nvim", blacklist = {}},
     -- onedarkpro
-    {
-      "olimorris/onedarkpro.nvim",
-      blacklist = { "onedark_dark", "onedark_vivid", "onelight" },
-    },
+    {"olimorris/onedarkpro.nvim", blacklist = {"onedark", "onelight"}},
+    -- onemonokai
+    {"cpea2506/one_monokai.nvim", blacklist = {}},
     -- onenord
-    {
-      "rmehri01/onenord.nvim",
-      blacklist = { "onenord-light" },
-    },
+    {"rmehri01/onenord.nvim", blacklist = {"onenord-light"}},
     -- rose-pine
-    {
-      "rose-pine/neovim",
-      blacklist = { "rose-pine", "rose-pine-dawn" },
-    },
+    {"rose-pine/neovim", blacklist = {"rose-pine", "rose-pine-dawn"}},
     -- tokyodark
-    {
-      "tiagovla/tokyodark.nvim",
-      blacklist = {},
-    },
+    {"tiagovla/tokyodark.nvim", blacklist = {}},
     -- tokyonight
-    {
-      "folke/tokyonight.nvim",
-      blacklist = { "tokyonight", "tokyonight-day" },
-    },
+    {"folke/tokyonight.nvim", blacklist = {"tokyonight", "tokyonight-day"}},
+    -- vnnight
+    {"nxvu699134/vn-night.nvim", blacklist = {}},
   },
 }

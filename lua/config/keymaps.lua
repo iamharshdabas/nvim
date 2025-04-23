d322b57f297e768f4f5e512f501df3eb8c19b2d2
Harsh Dabas <iamharshdabas@gmail.com>
2025-04-23 14:42:48 +0000

    fix: remove neovide code

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,11 +6,3 @@ set("n", "<S-x>", function()
 end, { desc = "Delete Buffer" })
 
 set("n", "<leader>t", "<CMD>Themify<CR>", { desc = "Themify" })
-
-if vim.g.neovide then
-  set("v", "<C-S-c>", '"+y') -- Copy
-  set("n", "<C-S-v>", '"+P') -- Paste normal mode
-  set("v", "<C-S-v>", '"+P') -- Paste visual mode
-  set("c", "<C-S-v>", "<C-R>+") -- Paste command mode
-  set("i", "<C-S-v>", '<ESC>l"+Pli') -- Paste insert mode
-end
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,12 +1,5 @@
 local opt = vim.opt
-local g = vim.g
 
 opt.cursorline = false
 opt.sidescrolloff = 16
 opt.scrolloff = 8
-opt.guifont = "FiraCode Nerd Font Mono:h14"
-
-g.neovide_transparency = 0.8
-g.neovide_hide_mouse_when_typing = true
-g.neovide_cursor_trail_size = 0.8
-g.neovide_cursor_vfx_mode = "pixiedust"

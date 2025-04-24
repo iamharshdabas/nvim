5057c7780fdd8596cf791b08231bf2b12e9a774d
Harsh Dabas <iamharshdabas@gmail.com>
2025-04-24 19:12:33 +0530

    fix: transparent themes

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,57 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    opts = { transparent_background = true },
+  },
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = { transparent = true },
+  },
+  {
+    "AlexvZyl/nordic.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = {
+        bg = true,
+        float = true,
+      },
+    },
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      styles = {
+        transparency = true,
+      },
+    },
+  },
+  {
+    "tiagovla/tokyodark.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = { transparent = true },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyodark",
+    },
+  },
+}

fcebce8b2cbe5451eac8af686beb6181391decb8
Harsh Dabas <iamharshdabas@gmail.com>
2025-04-29 09:31:36 +0530

    fix: transparent tokyonight sidebar

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -46,7 +46,13 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    opts = { transparent = true },
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
   },
   {
     "LazyVim/LazyVim",

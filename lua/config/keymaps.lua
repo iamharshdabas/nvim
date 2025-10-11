f2fe51d44efd36b5405170afbc9a25c98ef98cca
Harsh Dabas <iamharshdabas@gmail.com>
2025-10-11 15:10:14 +0530

    feat: add referencer plugin and keymap for toggling virtual text

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,8 @@ set("n", "<leader>rr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], {
 set("n", "<S-x>", function()
   Snacks.bufdelete()
 end, { desc = "Delete Buffer" })
+
+-- Toggle referencer virtual text (shows reference counts)
+set("n", "<leader>uR", function()
+  vim.cmd("ReferencerToggle")
+end, { desc = "Toggle Referencer" })

9ee1e000822bfc0e6f28114ebf2431def4660d06
Harsh Dabas <iamharshdabas@gmail.com>
2025-10-26 11:06:34 +0530

    fix: bun hot/watch not working

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,6 +4,7 @@ local g = vim.g
 opt.cursorline = false
 opt.sidescrolloff = 16
 opt.scrolloff = 8
+opt.backupcopy = "yes" -- for  bun --hot/watch to work properly
 
 g.neovide_opacity = 0.8
 g.neovide_hide_mouse_when_typing = true
